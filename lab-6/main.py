475ebb485ccc45e86fdefcf513c9e4ba149514a2
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2022-05-25 19:13:36 +0200

    Initial Q-Learning implementation

--- lab-6/main.py
+++ lab-6/main.py
@@ -1,13 +1,36 @@
+# Author: Jakub Mazurkiewicz
+from argparse import ArgumentParser
 import gym
+import qlearning as ql
+
+def parse_hyperparameters():
+    params = ql.Hyperparameters()
+    argparser = ArgumentParser(description='Taxi problem and QLearning algorithm')
+    argparser.add_argument('-lr', '--learning-rate',
+                           dest='learning_rate', type=float, default=params.learning_rate,
+                           help=f'Learning rate (default: {params.learning_rate})')
+    argparser.add_argument('-df', '--discount-factor',
+                           dest='discount_factor', type=float, default=params.discount_factor,
+                           help=f'Discount factor (default: {params.discount_factor})')
+    argparser.add_argument('-e', '--episodes',
+                           dest='episodes', type=int, default=params.episodes,
+                           help=f'Number of episodes (default: {params.episodes})')
+    argparser.add_argument('-eps', '--epsilon',
+                           dest='epsilon', type=float, default=params.epsilon,
+                           help=f'Epsilon for epsilon-greedy evaluation (default: {params.epsilon})')
+    args = argparser.parse_args()
+    params.learning_rate = args.learning_rate
+    params.discount_factor = args.discount_factor
+    params.epsilon = args.epsilon
+    params.episodes = args.episodes
+    return params
 
 if __name__ == '__main__':
-    env = gym.make("LunarLander-v2", render_mode="rgb_array")
-    observation, info = env.reset(seed=0xDEADBEEF, return_info=True)
-
-    for _ in range(1000):
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            observation, info = env.reset(return_info=True)
-
-    env.close()
+    # Environment:
+    # * Blue letter - passenger pick-up location
+    # * Purple letter - passenger's destination
+    env = gym.make("Taxi-v3")
+    algo = ql.QLearning(env, parse_hyperparameters())
+    algo.learn()
+    algo.evaluate()
+    algo.play()
--- lab-6/qlearning.py
+++ lab-6/qlearning.py
@@ -1,3 +1,71 @@
+# Author: Jakub Mazurkiewicz
+from dataclasses import dataclass
+from time import sleep
+import numpy as np
+from gym import Env
+
+@dataclass
+class Hyperparameters:
+    learning_rate: float = 0.1
+    discount_factor: float = 0.6
+    epsilon: float = 0.1
+    episodes: int = 1000
+
+@dataclass
+
 class QLearning:
-    def __init__(self):
-        pass
+    def __init__(self, env: Env, hyperparameters: Hyperparameters):
+        self.qtable = np.zeros((env.observation_space.n, env.action_space.n))
+        self.env = env
+        self.params = hyperparameters
+
+    def learn(self, seed=None):
+        for _ in range(self.params.episodes):
+            seed = 0xABBA if seed is None else np.random.randint()
+            state = self.env.reset()
+            done = False
+            while not done:
+                action = self._epsilon_greedy_exploration(state)
+                next_state, reward, done, _ = self.env.step(action)
+                self.qtable[state, action] = self.qtable[state, action] + self.params.learning_rate * (
+                    reward + self.params.discount_factor * np.max(self.qtable[next_state]) - self.qtable[state, action])
+                state = next_state
+
+    def _epsilon_greedy_exploration(self, state):
+        if np.random.random() < self.params.epsilon:
+            return self.env.action_space.sample()
+        else:
+            return np.argmax(self.qtable[state])
+
+    def evaluate(self):
+        total_epochs = 0
+        total_penalties = 0
+
+        for _ in range(self.params.episodes):
+            state = self.env.reset()
+            epochs = 0
+            penalties = 0
+            reward = 0
+            done = False
+            while not done:
+                action = np.argmax(self.qtable[state])
+                state, reward, done, _ = self.env.step(action)
+                if reward < 0:
+                    penalties += 1
+                epochs += 1
+
+            total_epochs += epochs
+            total_penalties += penalties
+        print(f"Results after {self.params.episodes} episodes:")
+        print(f'Average timesteps per episode: {total_epochs / self.params.episodes}')
+        print(f'Average penalties per episode: {total_penalties / self.params.episodes}')
+
+
+    def play(self, seed=0xDEADBEEF, delay=1.0):
+        state = self.env.reset(seed=seed)
+        done = False
+        while not done:
+            self.env.render()
+            action = np.argmax(self.qtable[state])
+            state, _, done, _ = self.env.step(action)
+            sleep(delay)
