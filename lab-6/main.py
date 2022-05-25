eda407d5ebf5d2016cab4404a47fa81ed8b819eb
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2022-05-25 19:51:53 +0200

    More stats and initial documentation

--- lab-6/main.py
+++ lab-6/main.py
@@ -1,36 +1,71 @@
 # Author: Jakub Mazurkiewicz
 from argparse import ArgumentParser
+import random
 import gym
+import matplotlib.pyplot as plt
 import qlearning as ql
 
-def parse_hyperparameters():
+def parse_args():
     params = ql.Hyperparameters()
     argparser = ArgumentParser(description='Taxi problem and QLearning algorithm')
-    argparser.add_argument('-lr', '--learning-rate',
-                           dest='learning_rate', type=float, default=params.learning_rate,
-                           help=f'Learning rate (default: {params.learning_rate})')
-    argparser.add_argument('-df', '--discount-factor',
-                           dest='discount_factor', type=float, default=params.discount_factor,
-                           help=f'Discount factor (default: {params.discount_factor})')
-    argparser.add_argument('-e', '--episodes',
-                           dest='episodes', type=int, default=params.episodes,
-                           help=f'Number of episodes (default: {params.episodes})')
-    argparser.add_argument('-eps', '--epsilon',
-                           dest='epsilon', type=float, default=params.epsilon,
-                           help=f'Epsilon for epsilon-greedy evaluation (default: {params.epsilon})')
+    argparser.add_argument(
+        '-lr', '--learning-rate', dest='learning_rate', type=float, default=params.learning_rate,
+        help=f'Learning rate (default: {params.learning_rate})'
+    )
+    argparser.add_argument(
+        '-df', '--discount-factor', dest='discount_factor', type=float, default=params.discount_factor,
+        help=f'Discount factor (default: {params.discount_factor})'
+    )
+    argparser.add_argument(
+        '-e', '--episodes', dest='episodes', type=int, default=params.episodes,
+        help=f'Number of episodes (default: {params.episodes})'
+    )
+    argparser.add_argument(
+        '-eps', '--epsilon', dest='epsilon', type=float, default=params.epsilon,
+        help=f'Epsilon for epsilon-greedy evaluation (default: {params.epsilon})'
+    )
+    argparser.add_argument(
+        '-p', '--play', dest='should_play', action='store_true', default=False,
+        help='Should program play the game after learning (default: False)'
+    )
     args = argparser.parse_args()
     params.learning_rate = args.learning_rate
     params.discount_factor = args.discount_factor
     params.epsilon = args.epsilon
     params.episodes = args.episodes
-    return params
+    return params, args.should_play
 
 if __name__ == '__main__':
     # Environment:
     # * Blue letter - passenger pick-up location
     # * Purple letter - passenger's destination
     env = gym.make("Taxi-v3")
-    algo = ql.QLearning(env, parse_hyperparameters())
-    algo.learn()
-    algo.evaluate()
-    algo.play()
+
+    hyperparameters, should_play = parse_args()
+    print(hyperparameters)
+    algo = ql.QLearning(env, hyperparameters)
+    learning_stats = algo.learn()
+
+    plt.figure()
+    plt.scatter(range(1, hyperparameters.episodes+1), [stat.penalty_count for stat in learning_stats], s=3)
+    plt.xlabel('Numer epizodu')
+    plt.ylabel('Ilość przyznanych kar')
+    plt.title('Ilość przyznanych kar na epizod')
+    plt.grid(True)
+
+    plt.figure()
+    plt.scatter(range(1, hyperparameters.episodes+1), [stat.total_reward for stat in learning_stats], s=3)
+    plt.xlabel('Numer epizodu')
+    plt.ylabel('Suma nagród')
+    plt.title('Suma nagród w zależności od epizodu')
+    plt.grid(True)
+    plt.show()
+
+    algo.save_qtable('qtable.log')
+
+    evaluation_stats = algo.evaluate()
+    print(f'Average epochs per episode: {evaluation_stats.avg_epoch_count}')
+    print(f'Average penalty per episode: {evaluation_stats.avg_penalty}')
+
+    if should_play:
+        algo.play(seed=random.getrandbits(64))
--- lab-6/qlearning.py
+++ lab-6/qlearning.py
@@ -1,6 +1,8 @@
 # Author: Jakub Mazurkiewicz
 from dataclasses import dataclass
+import random
 from time import sleep
+from typing import List
 import numpy as np
 from gym import Env
 
@@ -11,25 +13,48 @@ class Hyperparameters:
     epsilon: float = 0.1
     episodes: int = 1000
 
+    def __repr__(self):
+        return '\n'.join([
+            f'Learning rate: {self.learning_rate}',
+            f'Discount factor: {self.discount_factor}',
+            f'Epsilon: {self.epsilon}',
+            f'Episodes: {self.episodes}'
+        ])
+
 @dataclass
+class LearningStats:
+    penalty_count: int = 0
+    total_reward: int = 0
+
+@dataclass
+class EvaluationStats:
+    avg_epoch_count: float = 0
+    avg_penalty: float = 0
 
 class QLearning:
-    def __init__(self, env: Env, hyperparameters: Hyperparameters):
-        self.qtable = np.zeros((env.observation_space.n, env.action_space.n))
-        self.env = env
+    def __init__(self, environment: Env, hyperparameters: Hyperparameters):
+        self.env = environment
         self.params = hyperparameters
+        self.qtable = np.zeros((self.env.observation_space.n, self.env.action_space.n))
 
-    def learn(self, seed=None):
+    def learn(self, seed=None) -> List[LearningStats]:
+        stat_list = []
         for _ in range(self.params.episodes):
-            seed = 0xABBA if seed is None else np.random.randint()
+            stats = LearningStats()
+            seed = 0xABBA if seed is None else random.getrandbits(64)
             state = self.env.reset()
             done = False
+
             while not done:
                 action = self._epsilon_greedy_exploration(state)
                 next_state, reward, done, _ = self.env.step(action)
                 self.qtable[state, action] = self.qtable[state, action] + self.params.learning_rate * (
                     reward + self.params.discount_factor * np.max(self.qtable[next_state]) - self.qtable[state, action])
                 state = next_state
+                stats.total_reward += reward
+                stats.penalty_count += 1 if reward < 0 else 0
+            stat_list += [stats]
+        return stat_list
 
     def _epsilon_greedy_exploration(self, state):
         if np.random.random() < self.params.epsilon:
@@ -37,7 +62,7 @@ class QLearning:
         else:
             return np.argmax(self.qtable[state])
 
-    def evaluate(self):
+    def evaluate(self) -> EvaluationStats:
         total_epochs = 0
         total_penalties = 0
 
@@ -56,10 +81,10 @@ class QLearning:
 
             total_epochs += epochs
             total_penalties += penalties
-        print(f"Results after {self.params.episodes} episodes:")
-        print(f'Average timesteps per episode: {total_epochs / self.params.episodes}')
-        print(f'Average penalties per episode: {total_penalties / self.params.episodes}')
-
+        return EvaluationStats(
+            avg_epoch_count=(total_epochs / self.params.episodes),
+            avg_penalty=(total_penalties / self.params.episodes)
+        )
 
     def play(self, seed=0xDEADBEEF, delay=1.0):
         state = self.env.reset(seed=seed)
@@ -69,3 +94,10 @@ class QLearning:
             action = np.argmax(self.qtable[state])
             state, _, done, _ = self.env.step(action)
             sleep(delay)
+        self.env.render()
+
+    def load_qtable(self, filename):
+        self.qtable = np.loadtxt(filename)
+
+    def save_qtable(self, filename):
+        np.savetxt(filename, self.qtable)
